321050b33134b198ad7de3cda93db643a490a80a
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2024-04-21 11:44:36 +0200

    Removes contact form

--- pages/contatti.vue
+++ pages/contatti.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
-    <ContactForm title="Richiedi informazioni" description="Compila il form per richiere informazioni o un preventivo" />
+    Contatti
+    <!-- <ContactForm title="Richiedi informazioni" description="Compila il form per richiere informazioni o un preventivo" /> -->
   </main>
 </template>
